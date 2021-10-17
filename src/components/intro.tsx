147617a300c00b2b3f6aadf85b26bb407fb39d5b
yjkim <yjkim1803@gmail.com>
2021-10-17 18:52:11 +0900

    feat: add animation at intro

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image';
 import { css, cx, keyframes } from '@emotion/css';
 import frameTop from 'public/assets/images/frame_top.png';
 import frameBottom from 'public/assets/images/frame_bottom.png';
+import character from 'public/assets/images/charactor.png';
+import spotlight from 'public/assets/images/spotlight.png';
+import thinking from 'public/assets/images/thinking.png';
 
 const animation = {
   fadeIn: keyframes({
@@ -11,6 +14,28 @@ const animation = {
     },
     to: {
       opacity: 1,
+    },
+  }),
+  fadeInOut: keyframes({
+    from: {
+      opacity: 0,
+    },
+    '30%': {
+      opacity: 1,
+    },
+    '70%': {
+      opacity: 1,
+    },
+    to: {
+      opacity: 0,
+    },
+  }),
+  smaller: keyframes({
+    from: {
+      width: '15rem',
+    },
+    to: {
+      width: '5rem',
     },
   }),
 };
@@ -33,6 +58,42 @@ const styles = {
     width: '40rem',
     margin: '2rem 0',
   }),
+  characterWrapper: css({
+    width: '15rem',
+    position: 'relative',
+    height: '20rem',
+    animation: `${animation.smaller} 2s`,
+    animationDelay: '5.5s',
+    animationFillMode: 'both',
+  }),
+  character: css({
+    position: 'absolute',
+    bottom: '10%',
+    width: '75%',
+    left: 0,
+    right: 0,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    animation: `${animation.fadeIn} 1s`,
+    animationDelay: '0.5s',
+    animationFillMode: 'both',
+  }),
+  spotlight: css({
+    position: 'absolute',
+    bottom: '0',
+    animation: `${animation.fadeInOut} 4s`,
+    animationDelay: '2s',
+    animationFillMode: 'both',
+  }),
+  thinking: css({
+    position: 'absolute',
+    bottom: '8rem',
+    right: '-6rem',
+    width: '50%',
+    animation: `${animation.fadeInOut} 3s`,
+    animationDelay: '3s',
+    animationFillMode: 'both',
+  }),
 };
 
 const messages = [
@@ -50,28 +111,55 @@ const Intro: React.FC<Props> = ({ skip }) => {
   const [index, setIndex] = useState<number>(0);
 
   useEffect(() => {
+    let timeout: number = 0;
     const interval = window.setInterval(() => {
       if (index < messages.length - 1) {
         setIndex((idx) => idx + 1);
       } else {
-        skip();
+        timeout = window.setTimeout(() => {
+          skip();
+        }, 5000);
       }
     }, 2000);
-    return () => window.clearInterval(interval);
+    return () => {
+      window.clearInterval(interval);
+      window.clearTimeout(timeout);
+    };
   }, [index, skip]);
+
+  const isMessageEnd = index === messages.length - 1;
 
   return (
     <>
       <div className={cx([styles.wrapper])}>
-        <span className={cx([styles.frame])}>
-          <Image src={frameTop} alt="frame" />
-        </span>
-        <p className={styles.text} key={index}>
-          {messages[index]}
-        </p>
-        <span className={styles.frame}>
-          <Image src={frameBottom} alt="frame" />
-        </span>
+        {
+          isMessageEnd
+            ? (
+              <div className={styles.characterWrapper}>
+                <span className={styles.character}>
+                  <Image src={character} alt="character" />
+                </span>
+                <span className={styles.spotlight}>
+                  <Image src={spotlight} alt="spotlight" />
+                </span>
+                <span className={styles.thinking}>
+                  <Image src={thinking} alt="thinking" />
+                </span>
+              </div>
+            ) : (
+              <>
+                <span className={cx([styles.frame])}>
+                  <Image src={frameTop} alt="frame" />
+                </span>
+                <p className={styles.text} key={index}>
+                  {messages[index]}
+                </p>
+                <span className={styles.frame}>
+                  <Image src={frameBottom} alt="frame" />
+                </span>
+              </>
+            )
+        }
       </div>
     </>
   );
